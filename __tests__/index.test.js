5aa5f44e4e17e4b91157e7802639ed5388e0671c
szittyu <szittyu1@gmail.com>
2022-06-03 16:20:13 +0200

    header added to home page

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
-describe('Home', () => {
+describe('Home page', () => {
+
+
     it('renders a heading', () => {
         render(<Home />)
 
         const heading = screen.getByRole('heading', {
-            name: /welcome to next\.js!/i,
+            name: /welcome to my page/i,
         })
 
         expect(heading).toBeInTheDocument()
