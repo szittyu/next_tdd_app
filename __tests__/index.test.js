0714939b0469821408b9b5fd53f9d3d8f11cc4d8
szittyu <szittyu1@gmail.com>
2022-06-03 22:31:26 +0200

    commerce made

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,25 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { getByRole, render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
-describe('Home page', () => {
+describe("Home page", () => {
 
 
-    it('renders a heading', () => {
+    it("renders a heading", () => {
         render(<Home />)
-
-        const heading = screen.getByRole('heading', {
+        const heading = screen.getByRole("heading", {
             name: /welcome to my page/i,
         })
-
         expect(heading).toBeInTheDocument()
     })
+
+    it("render an accessible search box", () => {
+        render(<Home />)
+        const searchInput = screen.getByRole("searchbox")
+        expect(searchInput).toHaveAccessibleName("Search")
+    })
+
+    // it("renders all products", () => {
+
+    // })
 })
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
+import React from 'react'
+import commerce from '../lib/commerce'
 import Head from 'next/head'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  const { data: products } = await commerce.products.list()
+  const { data: categories } = await commerce.categories.list()
+
+  return {
+    props: { products, categories },
+    revalidate: 30
+  }
+}
+
+
+export default function Home({ products, categories }) {
+  console.log(products, categories)
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <Head>
         <title>TDD App | HOME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-center text-4xl mb-10 font-medium">Welcome to my page</h1>
+      <h1 className="text-4xl mb-10 font-medium">Welcome to my page</h1>
+      <input
+        role="searchbox"
+        type="text"
+        title="Search"
+        placeholder="Search products" />
 
     </div>
   )
