6090b2b34bce2c693ff7f0dc39a69785281e8c70
szittyu <szittyu1@gmail.com>
2022-06-07 12:47:01 +0200

    index tests fully made

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,35 +1,67 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import Home from '../pages/index'
+import userEvent from '@testing-library/user-event'
+import { mockProducts } from '../__mocks__/mockProduct'
+import { mockCategories } from '../__mocks__/mockData'
 
 describe("Home page", () => {
+    //
 
+    it("render  all products heading", () => {
+        render(<Home products={mockProducts} categories={[]} />);
+        screen.getByRole("heading", { name: /All products/i });
+    });
 
-    it("renders a heading", () => {
-        render(<Home />)
-        const heading = screen.getByRole("heading", {
-            name: /welcome to my page/i,
-        })
-        expect(heading).toBeInTheDocument()
-    })
+    //
 
-    it("render an accessible search box", () => {
-        render(<Home />)
-        const searchInput = screen.getByRole("searchbox")
-        expect(searchInput).toHaveAccessibleName("Search")
-    })
+    it("render a accessible search box", () => {
+        render(<Home products={mockProducts} categories={[]} />);
+        const searchInput = screen.getByRole("searchbox");
+        expect(searchInput).toHaveAccessibleName("Search");
+    });
+
+    //
 
     it("renders all products", () => {
-        render(<Home products={[]} categories={[]} />)
+        render(<Home products={mockProducts} categories={[]} />);
+        const allproducts = screen.getByRole("list", { name: /products/i });
+        const { getAllByRole } = within(allproducts);
+        const listitems = getAllByRole("listitem");
+        expect(listitems.length).toBe(3);
     });
-    // it("renders all categories", () => {
 
+    //
+
+    it("renders all categories", () => {
+        render(<Home products={mockProducts} categories={mockCategories} />);
+        const allcategories = screen.getByRole("list", { name: /categories/i });
+        const { getAllByRole } = within(allcategories);
+        const listitems = getAllByRole("listitem", { name: /category/i });
+        expect(listitems.length).toBe(2);
+    });
+
+    // it("renders 1 product per  category", () => {
+    //   render(<Home products={mockProducts} categories={mockCategories} />);
+    //   const categorylist = screen.getByRole("list", { name: /Mouse/i });
+    //   const { getAllByRole } = within(categorylist);
+    //   const listitems = getAllByRole("listitem");
+    //   expect(listitems.length).toBe(1);
     // });
-    // it("renders 1 product per category", () => {
 
-    // });
-    // it("show search results when search item is not empty", () => {
+    it("show only search results when search term is not empty", async () => {
+        render(<Home products={mockProducts} categories={mockCategories} />);
+        const searchBox = screen.getByPlaceholderText(/Search products/i);
+        await userEvent.type(searchBox, "mo");
 
-    // });
+        const searchResults = screen.getByRole("list", { name: /search results/i });
+        expect(searchResults).toBeVisible();
 
-})
+        const { getAllByRole } = within(searchResults);
+        const listitems = getAllByRole("listitem");
+        expect(listitems.length).toBe(2);
+
+        const allproducts = screen.queryByRole("list", { name: /products/i });
+        expect(allproducts).toBeNull();
+    });
+});
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import commerce from '../lib/commerce'
 import Head from 'next/head'
 
@@ -14,21 +14,75 @@ export async function getStaticProps(context) {
 
 
 export default function Home({ products, categories }) {
-  console.log(products, categories)
+  const [searchTerm, setSearchTerm] = useState("")
   return (
     <div className="flex flex-col justify-center items-center">
       <Head>
         <title>TDD App | HOME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        <input
+          className="my-10 border-gray-500 border-2 rounded-lg"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          role="searchbox"
+          type="text"
+          title="Search"
+          placeholder="Search products"
+        />
 
-      <h1 className="text-4xl mb-10 font-medium">Welcome to my page</h1>
-      <input
-        role="searchbox"
-        type="text"
-        title="Search"
-        placeholder="Search products" />
+        {searchTerm.length > 0 ? (
+          <>
+            <h2
+              id="search-results-heading"
+              className="text-xl font-bold"
+            >Search results</h2>
+            <ul aria-labelledby="search-results-heading" className="mt-10">
+              {products
+                .filter((product) =>
+                  product.name.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((product) => {
+                  return <li key={product.id} className="list-disc">{product.name}</li>;
+                })}
+            </ul>
+          </>
+        ) : (
+          <>
+            <ul aria-label="Categories">
+              {categories.map((category) => {
+                return (
+                  <li aria-label="category" key={category.id}>
+                    <h2 id={`category-${category.name}`} className="text-xl font-bold my-2">{category.name}</h2>
+                    <ul aria-labelledby={`category-${category.name}`}>
+                      {products
+                        .filter((product) =>
+                          product.categories.find((c) => c.id === category.id)
+                        )
+                        .map((product) => {
+                          return <li key={product.id} className="list-disc">{product.name}</li>;
+                        })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
 
+            <h2
+              id="all-products-heading"
+              className="text-xl my-10 font-bold"
+            >All Products</h2>
+            <ul aria-labelledby="all-products-heading">
+              {products.map((product) => {
+                return <li key={product.id} className="list-disc">{product.name}</li>;
+              })}
+            </ul>
+          </>
+        )}
+      </main>
     </div>
   )
 }
