317906dbb40b13aa910424aef94eea83e01eaac3
szittyu <szittyu1@gmail.com>
2022-07-05 22:08:55 +0200

    footer responsive

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return (
         <footer
-            className="flex flex-row justify-end mr-10 my-10"
+            className="flex flex-row justify-center mx-2 my-5 md:justify-end md:mr-10 md:my-10"
         >
             <p>&copy; 2022 Online Store · All rights reserved</p>
         </footer>
