f4b28775ca5555984f7a4ff10bccec032da63beb
szittyu <szittyu1@gmail.com>
2022-07-06 15:20:47 +0200

    payment responsivity made

--- components/Payment/Payment.js
+++ components/Payment/Payment.js
@@ -70,12 +70,14 @@ export default function Payment({ shippingData, checkoutToken, refreshCart }) {
         <div className="flex flex-col items-center justify-center w-full rounded-[60px] bg-gray-100">
             {order ? (
                 <>
-                    <h3 className="mt-10">
-                        Thanks for ordering&nbsp;
+                    <h3 className="my-10 text-center text-xl">
+                        Thanks for ordering! <br></br>
                         <span className="font-medium">
                             {order.customer.firstname} {order.customer.lastname}&nbsp;
                         </span>
-                        <span>your order reference:
+                        <br></br>
+                        <span className="text-base">Your order reference:
+                            <br></br>
                             <span className="font-medium"> {order.customer_reference}</span>
                         </span>
                     </h3>
@@ -90,8 +92,8 @@ export default function Payment({ shippingData, checkoutToken, refreshCart }) {
                 </>
             ) : (
                 <>
-                    <h1 className="flex justify-center items-center text-3xl font-bold my-10">Give your card details!</h1>
-                    <div className="flex flex-col justify-center items-start font-bold w-3/5">
+                    <h1 className="flex justify-center items-center text-2xl font-bold my-10 md:text-3xl">Give your card details!</h1>
+                    <div className="flex flex-col justify-center items-start font-bold w-4/5 md:w-4/5 lg:w-3/5">
                         <p>Full name: <span className="font-medium">{shippingData.firstname} {shippingData.lastname}</span></p>
                         <p>Address: <span className="font-medium">{shippingData.address1}</span></p>
                         <p>Email: <span className="font-medium">{shippingData.email}</span></p>
@@ -100,31 +102,32 @@ export default function Payment({ shippingData, checkoutToken, refreshCart }) {
                         <p> Country Code: <span className="font-medium">{shippingData.country}</span></p>
                         <p> Subdivision Code: <span className="font-medium">{shippingData.subdivisions}</span></p>
                     </div>
-                    <h4 className="text-xl font-bold my-5">Items you are buying:</h4>
-                    <div className="w-3/5 font-medium">
+                    <h4 className="text-lg font-bold my-5 md:text-xl">Items you are buying:</h4>
+                    <div className="w-4/5 font-medium text-sm md:text-base md:w-4/5 lg:w-3/5">
                         <ul>
                             {checkoutToken.live.line_items.map((item) => {
                                 return (
                                     <p key={item.product_name}>
-                                        {item.product_name} - {item.line_total.formatted_with_code}{" "}
+                                        {item.product_name} - {item.line_total.formatted_with_code}{" "}<br></br>
                                         (Quantity: {item.quantity} )
                                     </p>
                                 );
                             })}
                         </ul>
-                        <p className="mb-5 font-bold">
-                            Total : {checkoutToken.live.total.formatted_with_code} +
-                            {shippingcharges.price.formatted_with_code}(Shipping Charges)
+                        <p className="my-5 font-bold text-sm w-full md:text-base md:w-4/5">
+                            Total: {checkoutToken.live.total.formatted_with_code} + {shippingcharges.price.formatted_with_code} <br></br>(Shipping Charges)
+
+
                         </p>
                     </div>
 
                     {error && <h3>Error : {error}</h3>}
                     {loading ? (
-                        <p className="flex flex-row justify-center items-center w-full h-36 font-bold text-2xl">Wait while we process your data...</p>
+                        <p className="text-center w-full h-36 font-bold text-xl md:text-2xl">Wait while we process your data...</p>
                     ) : (
                         <form
                             onSubmit={handleSubmit(payNow)}
-                            className="flex flex-col justify-between items-center font-medium w-4/5 my-2"
+                            className="flex flex-col justify-between items-center text-sm font-medium w-full my-2 lg:w-5/6 md:text-base"
                         >
                             <FormInput
                                 param={{
@@ -162,7 +165,7 @@ export default function Payment({ shippingData, checkoutToken, refreshCart }) {
                                 register={register}
                             />
                             <button
-                                className="border-2 border-black rounded-full w-24 h-10 my-10 hover:bg-black transition duration-300 hover:text-white"
+                                className="border-2 border-black rounded-full text-xs w-20 h-10 my-10 hover:bg-black transition duration-300 hover:text-white md:w-24 md:text-base"
                                 type="submit"
                             >
                                 Pay Now
