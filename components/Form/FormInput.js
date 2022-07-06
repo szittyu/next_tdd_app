dbb50c60791de1bf18d12c5070b7cab240096325
szittyu <szittyu1@gmail.com>
2022-07-06 13:58:11 +0200

    forminput responsivity made

--- components/Form/FormInput.js
+++ components/Form/FormInput.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FormInput({ param, register }) {
     return (
-        <div className="flex flex-row justify-between items-center font-medium w-2/6 my-2"
+        <div className="flex flex-row justify-between items-center font-medium w-5/6 my-2 md:w-3/6 lg:w-2/6"
         >
             <label htmlFor={param.name}>{param.name}:</label>
             <input
